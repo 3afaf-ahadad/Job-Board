d6f82bc7719026336af72da4ae5c4f70753d1bfe
Afaf <afaf20ahadad@gmail.com>
2026-03-28 16:32:44 +0000

    feat: relationships between User and Listing models

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Listing extends Model
 {
-    //
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
