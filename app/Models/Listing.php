3e3c4ad44cafad1f0ee18a4a044c8ff0d0cd6d09
Afaf <afaf20ahadad@gmail.com>
2026-03-28 22:46:43 +0000

    feat: listings controller; index

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Listing extends Model
 {
+    protected $fillable = ['title', 'desc' ];
     public function users(): BelongsTo
     {
         return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('listings', ListingController::class);
