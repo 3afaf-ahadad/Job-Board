8d40ee1dbfd0d17e3044af8f77a6db54096934e5
Afaf <afaf20ahadad@gmail.com>
2026-03-28 16:09:31 +0000

    feat: filled the apps table

--- database/migrations/2026_03_28_155439_create_applications_table.php
+++ database/migrations/2026_03_28_155439_create_applications_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('listing_id')->constrained();
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
