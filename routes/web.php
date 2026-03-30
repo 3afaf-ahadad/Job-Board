f34823f518aab577188626026d7ce348ec46fca8
Afaf <afaf20ahadad@gmail.com>
2026-03-30 08:41:33 +0000

    fix: routes & feat: added a seeder

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::resource('listings', ListingController::class);
