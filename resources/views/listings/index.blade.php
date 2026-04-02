f295265da6674edd74d4754edb3a2ff8ce63ae80
Afaf <afaf20ahadad@gmail.com>
2026-04-02 17:02:45 +0000

    feat: added a basic layout

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>JobBoard | Home</title>
-
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-</head>
-
-<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<x-layout>
     @if(count($listings) == 0)
     <p>No jobs here...</p>
     @else
@@ -26,6 +11,4 @@
     <p> {{$job->desc}} </p>
     @endforeach
     @endif
-</body>
-
-</html>
+</x-layout>
--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JobBoard | Job Details</title>
-</head>
-
-<body>
+<x-layout>
     <h1> {{$listing->title}} </h1>
-    <h3> Job poster : {{$listing->user_id}} | Salary : {{$listing->salary}} </h3>
+    <h3> Salary : {{$listing->salary}} </h3>
     <p> {{$listing->desc}} </p>
-</body>
-
-</html>
+</x-layout>
