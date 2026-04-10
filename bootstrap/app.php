947b0b0d87c7d0a03774bf57f0b28ae6df852dc1
Afaf <afaf20ahadad@gmail.com>
2026-04-10 17:22:47 +0000

    refactor: update auth redirecting to listings in bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectTo(
+            guests: '/login',
+            users: '/listings'
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
