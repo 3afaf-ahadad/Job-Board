d0b73278f41ec61e4536dd8c10d5738f1a4e14c6
Afaf <afaf20ahadad@gmail.com>
2026-04-11 10:58:13 +0000

    fix : use firstOrCreate on User model instead of a factorry in DatabaseSeeder

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,26 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/listings', [ListingController::class, 'index'])->name('listings.index');
-Route::get('/listings/{id}', [ListingController::class, 'show'])->name('listings.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/listings/create', [ListingController::class, 'create'])->name('listings.create');
     Route::post('/listings/store', [ListingController::class, 'store'])->name('listings.store');
-
-    Route::get('/listings/{id}/edit', [ListingController::class, 'edit'])->name('listings.edit');
-    Route::delete('/listings/{id}', [ListingController::class, 'destroy'])->name('listings.destroy');
-    Route::put('/listings/{id}', [ListingController::class, 'update'])->name('listings.update');
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/listings/{id}/edit', [ListingController::class, 'edit'])->name('listings.edit');
+    Route::delete('/listings/{id}', [ListingController::class, 'destroy'])->name('listings.destroy');
+    Route::put('/listings/{id}', [ListingController::class, 'update'])->name('listings.update');
     });
     
+Route::get('/listings/{id}', [ListingController::class, 'show'])->name('listings.show');
+    
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+        require __DIR__ . '/auth.php';
+        
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
